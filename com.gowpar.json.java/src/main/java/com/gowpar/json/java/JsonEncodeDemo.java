5ef1b84fcab201f9f96fea49115d7af3171bc0a1
Marlon <martianworm17@gmail.com>
2018-07-09 12:37:36 +0800

    Encoding JSON in Java

---
+++ com.gowpar.json.java/src/main/java/com/gowpar/json/java/JsonEncodeDemo.java
@@ -0,0 +1,18 @@
+package com.gowpar.json.java;
+
+import org.json.simple.JSONObject;
+
+public class JsonEncodeDemo {
+	
+	public static void main(String[] args) {
+		JSONObject obj = new JSONObject();
+		
+		obj.put("name", "foo");
+		obj.put("num", new Integer(100));
+		obj.put("balance", new Double(1000.21));
+		obj.put("is_vip", new Boolean(true));
+		
+		System.out.println(obj);
+	}
+
+}
