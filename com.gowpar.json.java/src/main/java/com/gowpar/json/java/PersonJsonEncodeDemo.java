83eb76d03f897089a92ad2b8a24fd064327a4ee3
Marlon <martianworm17@gmail.com>
2018-07-24 15:11:30 +0800

    Build JSON Object from POJO

---
+++ com.gowpar.json.java/src/main/java/com/gowpar/json/java/PersonJsonEncodeDemo.java
@@ -0,0 +1,44 @@
+package com.gowpar.json.java;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PersonJsonEncodeDemo {
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+
+		person.setFirstName("Naruto");
+		;
+		person.setMiddleName("Shinobi");
+		person.setLastName("Uzumaki");
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			// Convert object to JSON string and save into a file directly
+			mapper.writeValue(new File("person.json"), person);
+
+			// Convert object to JSON string
+			String jsonInString = mapper.writeValueAsString(person);
+			System.out.println(jsonInString);
+
+			// Convert object to JSON string and pretty print
+			jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(person);
+			System.out.println(jsonInString);
+
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
